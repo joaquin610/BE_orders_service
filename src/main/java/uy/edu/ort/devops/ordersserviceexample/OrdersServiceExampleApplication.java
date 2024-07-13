c604f4069fe1d0b2d069cc2184b5a74b62081eaf
joaquin610 <joaquinbanchero610@hotmail.com>
2024-07-13 03:58:38 -0300

    chore: Add initial project files and structure

---
+++ src/main/java/uy/edu/ort/devops/ordersserviceexample/OrdersServiceExampleApplication.java
@@ -0,0 +1,30 @@
+package uy.edu.ort.devops.ordersserviceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import uy.edu.ort.devops.ordersserviceexample.logic.OrdersLogic;
+
+@SpringBootApplication
+public class OrdersServiceExampleApplication {
+
+	public static void main(String[] args) {
+		if (args.length > 0) {
+		OrdersLogic.setPaymentsServiceUrl(args[0]);
+		OrdersLogic.setShippingServiceUrl(args[1]);
+		OrdersLogic.setProductsServiceUrl(args[2]);
+
+		SpringApplication.run(OrdersServiceExampleApplication.class, args);
+		} else {
+			System.out.println("al llamar a main no paso argumentos para setear");
+		}
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+}
